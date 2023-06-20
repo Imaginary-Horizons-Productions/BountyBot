f9e2fd45f89d24913e0f7e15445f782b8cc3a61d
Nathaniel Tseng <nathanieltseng@gmail.com>
2023-06-20 12:55:10 -0700

    more wiki improvements

--- source/scripts/updateWiki.js
+++ source/scripts/updateWiki.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const { commandSets } = require('../commands/_commandDictionary.js');
 const { CommandWrapper } = require('../classes/InteractionWrapper.js');
+const { SlashCommandSubcommandBuilder } = require('discord.js');
 
 let text = "";
 
@@ -11,11 +12,17 @@ commandSets.forEach(commandSet => {
 		const command = require(`./../commands/${filename}`);
 		text += `### /${command.customId}\n> Cooldown: ${command.cooldown / 1000} second(s)\n\n${command.description}\n`;
 		for (const optionData of command.data.options) {
-			text += `#### ${optionData.name}${optionData.required ? "" : " (optional)"}\n`;
-			if (optionData.choices?.length > 0) {
-				text += `> Choices: ${optionData.choices.map(choice => choice.name).join(", ")}\n`;
+			let optionName = "#### ";
+			if (optionData instanceof SlashCommandSubcommandBuilder) {
+				optionName += `/${command.customId} ${optionData.name}\n`;
+			} else {
+				optionName += `${optionData.name}${optionData.required ? "" : " (optional)"}\n`;
 			}
-			text += `\n${optionData.description}\n`;
+			text += optionName;
+			if (optionData.choices?.length > 0) {
+				text += `> Choices: \`${optionData.choices.map(choice => choice.name).join("`, `")}\`\n\n`;
+			}
+			text += `${optionData.description}\n`;
 		}
 	})
 })
