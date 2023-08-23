c0fc550048f47be969551d9cee54be7e65cec3e5
Nathaniel Tseng <nathanieltseng@gmail.com>
2023-08-23 08:53:42 -0700

    fixes through /bounty take-down

--- source/selects/bountytakedown.js
+++ source/selects/bountytakedown.js
@@ -7,11 +7,11 @@ module.exports = new InteractionWrapper(customId, 3000,
 	/** Take down the given bounty and completions */
 	async (interaction, args) => {
 		const [slotNumber] = interaction.values;
-		const bounty = await database.models.Bounty.findOne({ where: { userId: interaction.user.id, companyId: interaction.guildId, slotNumber, state: "open" } });
+		const bounty = await database.models.Bounty.findOne({ where: { userId: interaction.user.id, companyId: interaction.guildId, slotNumber, state: "open" }, include: database.models.Bounty.Company });
 		bounty.state = "deleted";
 		bounty.save();
 		database.models.Completion.destroy({ where: { bountyId: bounty.id } });
-		if (company.bountyBoardId) {
+		if (bounty.Company.bountyBoardId) {
 			const bountyBoard = await interaction.guild.channels.fetch(company.bountyBoardId);
 			const postingThread = await bountyBoard.threads.fetch(bounty.postingId);
 			postingThread.delete("Bounty taken down by poster");
