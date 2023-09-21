929d61ec1c709186d30828b9faa0dce146e45d50
Nathaniel Tseng <nathanieltseng@gmail.com>
2023-09-21 10:22:24 -0700

    #99: Port `/config-server` (#105)

---
+++ source/commands/config-server.js
@@ -0,0 +1,50 @@
+const { PermissionFlagsBits } = require('discord.js');
+const { CommandWrapper } = require('../classes');
+const { database } = require('../../database');
+
+const mainId = "config-server";
+const options = [
+	{
+		type: "String",
+		name: "notification",
+		description: "Configure who to send notifications to",
+		required: false,
+		choices: [
+			{ name: "Notify online members (@here)", value: "@here" },
+			{ name: "Notify all members (@everyone)", value: "@everyone" },
+			{ name: "No prefix", value: "(nothing)" },
+			{ name: "Suppress notifications (@silent)", value: "@silent" }
+		]
+	},
+	{
+		type: "Boolean",
+		name: "server-boost-xp",
+		description: "Configure whether boosting the server awards XP",
+		required: false
+	}
+];
+const subcommands = [];
+module.exports = new CommandWrapper(mainId, "Configure BountyBot settings for this server", PermissionFlagsBits.ManageGuild, false, false, 3000, options, subcommands,
+	(interaction) => {
+		const company = database.models.Company.findByPk(interaction.guildId).then(company => {
+
+			const updatePayload = {};
+			let content = "The following server settings have been configured:";
+
+			const prefix = interaction.options.getString(options[0].name);
+			if (prefix !== null) {
+				updatePayload.announcementPrefix = prefix === "(nothing)" ? "" : prefix;
+				content += `\n- The announcment prefix was set to ${prefix}`;
+			}
+
+			const shouldBoostXP = interaction.options.getBoolean(options[1].name);
+			if (shouldBoostXP !== null) {
+				updatePayload.disableBoostXP = !shouldBoostXP;
+				content += `\n- XP for Server Boosts has been ${shouldBoostXP ? "en" : "dis"}abled`;
+			}
+
+			company.update(updatePayload);
+			interaction.reply({ content, ephemeral: true });
+		});
+	}
+);
