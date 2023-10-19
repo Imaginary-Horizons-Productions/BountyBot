54dacee19533f1abe3b18afd00126c5d30441f51
Nathaniel Tseng <nathanieltseng@gmail.com>
2023-10-19 11:11:37 -0700

    simplify embed tip storage

--- source/embedHelpers.js
+++ source/embedHelpers.js
@@ -11,31 +11,31 @@ const discordIconURL = "https://cdn.discordapp.com/attachments/61852387618757018
 const bountyBotIcon = "https://cdn.discordapp.com/attachments/618523876187570187/1138968614364528791/BountyBotIcon.jpg";
 /** @type {import("discord.js").EmbedFooterData[]} */
 const discordTips = [
-	{ text: "Message starting with @silent don't send notifications; good for when everyone's asleep.", iconURL: discordIconURL },
-	{ text: "Surround your message with || to mark it a spoiler (not shown until reader clicks on it).", iconURL: discordIconURL },
-	{ text: "Surround a part of your messag with ~~ to add strikethrough styling.", iconURL: discordIconURL },
-	{ text: "Don't forget to check slash commands for optional arguments.", iconURL: discordIconURL },
-	{ text: "Some slash commands can be used in DMs, others can't.", iconURL: discordIconURL },
-	{ text: "Server subscriptions cost more on mobile because the mobile app stores take a cut.", iconURL: discordIconURL }
-];
+	"Message starting with @silent don't send notifications; good for when everyone's asleep.",
+	"Surround your message with || to mark it a spoiler (not shown until reader clicks on it).",
+	"Surround a part of your messag with ~~ to add strikethrough styling.",
+	"Don't forget to check slash commands for optional arguments.",
+	"Some slash commands can be used in DMs, others can't.",
+	"Server subscriptions cost more on mobile because the mobile app stores take a cut."
+].map(text => ({ text, iconURL: discordIconURL }));
 /** @type {import("discord.js").EmbedFooterData[]} */
 const applicationSpecificTips = [
-	{ text: "You can showcase one of your bounties once a week to increase its rewards.", iconURL: bountyBotIcon },
-	{ text: "Send bug reports or feature requests with the \"/feedback\".", iconURL: bountyBotIcon },
-	{ text: "Bounties can't be completed until 5 minutes after they've been posted. Don't make them too easy!", iconURL: bountyBotIcon },
-	{ text: "You get XP for posting a bounty, but lose that XP if it's taken down before it's completed.", iconURL: bountyBotIcon },
-	{ text: "You get XP when your bounties are completed. Thanks for posting!", iconURL: bountyBotIcon },
-	{ text: "You get more XP when a bigger group completes your bounties. Thanks for organizing!", iconURL: bountyBotIcon },
-	{ text: "Sometimes when you raise a toast to someone, it'll crit and grant you XP too!", iconURL: bountyBotIcon },
-	{ text: "Your chance for Critical Toast is lower when repeatedly toasting the same bounty hunters. Spread the love!", iconURL: bountyBotIcon },
-	{ text: "Users who can manage BountyBot aren't included in seasonal rewards to avoid conflicts of interest.", iconURL: bountyBotIcon },
-	{ text: "Anyone can post a bounty, even you!", iconURL: bountyBotIcon },
-	{ text: "Anyone can raise a toast, even you!", iconURL: bountyBotIcon },
-	{ text: "The Overjustification Effect means a small reward can be less motivating than no reward.", iconURL: bountyBotIcon },
-	{ text: "Manage bounties from within games with the Discord Overlay (default: Shift + Tab)!", iconURL: bountyBotIcon },
-	{ text: "Server level is based on total bounty hunter level--higher server level means better evergreen bounty rewards.", iconURL: bountyBotIcon },
-	{ text: "A bounty poster cannot complete their own bounty.", iconURL: bountyBotIcon }
-];
+	"You can showcase one of your bounties once a week to increase its rewards.",
+	"Send bug reports or feature requests with the \"/feedback\".",
+	"Bounties can't be completed until 5 minutes after they've been posted. Don't make them too easy!",
+	"You get XP for posting a bounty, but lose that XP if it's taken down before it's completed.",
+	"You get XP when your bounties are completed. Thanks for posting!",
+	"You get more XP when a bigger group completes your bounties. Thanks for organizing!",
+	"Sometimes when you raise a toast to someone, it'll crit and grant you XP too!",
+	"Your chance for Critical Toast is lower when repeatedly toasting the same bounty hunters. Spread the love!",
+	"Users who can manage BountyBot aren't included in seasonal rewards to avoid conflicts of interest.",
+	"Anyone can post a bounty, even you!",
+	"Anyone can raise a toast, even you!",
+	"The Overjustification Effect means a small reward can be less motivating than no reward.",
+	"Manage bounties from within games with the Discord Overlay (default: Shift + Tab)!",
+	"Server level is based on total bounty hunter level--higher server level means better evergreen bounty rewards.",
+	"A bounty poster cannot complete their own bounty."
+].map(text => ({ text, iconURL: bountyBotIcon }));
 const tipPool = applicationSpecificTips.concat(applicationSpecificTips, discordTips);
 
 /** twice as likely to roll an application specific tip as a discord tip */
@@ -43,9 +43,7 @@ function randomFooterTip() {
 	return tipPool[Math.floor(Math.random() * tipPool.length)];
 }
 
-/**
- * @param {Guild} guild
- */
+/** @param {Guild} guild */
 async function buildCompanyStatsEmbed(guild) {
 	const [company] = await database.models.Company.findOrCreate({ where: { id: guild.id }, defaults: { Season: { companyId: guild.id } }, include: database.models.Company.Season });
 	const seasonParticipants = (await database.models.SeasonParticipation.findAll({ where: { seasonId: company.seasonId }, include: database.models.SeasonParticipation.Hunter, order: [["xp", "DESC"]] })).map(participation => participation.Hunter);
