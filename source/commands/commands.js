9e1f00dcf870ffa4a0a769a41fd6456f5914f1a7
Nathaniel Tseng <nathanieltseng@gmail.com>
2023-11-08 10:49:30 -0800

    clean up link presentation in /commands and /data-policy

--- source/commands/commands.js
+++ source/commands/commands.js
@@ -6,6 +6,6 @@ const subcommands = [];
 module.exports = new CommandWrapper(mainId, "Get a link to BountyBot's commands page", null, false, true, 3000, options, subcommands,
 	/** Link the user to the repo Commands wiki page (automatically updated) */
 	(interaction) => {
-		interaction.reply({ content: "Here's a link to the BountyBot Commands page (automatically updated): https://github.com/Imaginary-Horizons-Productions/BountyBot/wiki/Commands", ephemeral: true });
+		interaction.reply({ content: "Here's a [link to the BountyBot Commands page](<https://github.com/Imaginary-Horizons-Productions/BountyBot/wiki/Commands>).", ephemeral: true });
 	}
 );
--- source/commands/data-policy.js
+++ source/commands/data-policy.js
@@ -6,6 +6,6 @@ const subcommands = [];
 module.exports = new CommandWrapper(mainId, "Get a link to BountyBot's data policy page", null, false, true, 3000, options, subcommands,
 	/** Link the user to the repo Data Policy wiki page (automatically updated) */
 	(interaction) => {
-		interaction.reply({ content: "Here's a link to the BountyBot Data Policy page (automatically updated): https://github.com/Imaginary-Horizons-Productions/BountyBot/wiki/Data-Policy", ephemeral: true });
+		interaction.reply({ content: "Here's a [link to the BountyBot Data Policy page](<https://github.com/Imaginary-Horizons-Productions/BountyBot/wiki/Data-Policy>).", ephemeral: true });
 	}
 );
