997defdef3d3afd60c05cf20e0efd6ee9f34758d
Nathaniel Tseng <nathanieltseng@gmail.com>
2024-02-16 10:57:27 -0800

    add deleteReply to `/bounty take-down`

--- source/commands/bounty/takedown.js
+++ source/commands/bounty/takedown.js
@@ -1,6 +1,8 @@
 const { CommandInteraction, ActionRowBuilder, StringSelectMenuBuilder } = require("discord.js");
 const { Sequelize } = require("sequelize");
 const { trimForSelectOptionDescription, getNumberEmoji } = require("../../util/textUtil");
+const { SKIP_INTERACTION_HANDLING } = require("../../constants");
+const { getRankUpdates } = require("../../util/scoreUtil");
 
 /**
  * @param {CommandInteraction} interaction
@@ -19,18 +21,50 @@ async function executeSubcommand(interaction, database, runMode, ...[posterId]) 
 			};
 		});
 
-		interaction.reply({
-			content: "If you'd like to change the title, description, image, or time of your bounty, you can use `/bounty edit` instead.",
-			components: [
-				new ActionRowBuilder().addComponents(
-					new StringSelectMenuBuilder().setCustomId("bountytakedown")
-						.setPlaceholder("Select a bounty to take down...")
-						.setMaxValues(1)
-						.setOptions(bountyOptions)
-				)
-			],
-			ephemeral: true
-		});
+		interaction.deferReply({ ephemeral: true }).then(() => {
+			return interaction.editReply({
+				content: "If you'd like to change the title, description, image, or time of your bounty, you can use `/bounty edit` instead.",
+				components: [
+					new ActionRowBuilder().addComponents(
+						new StringSelectMenuBuilder().setCustomId(`${SKIP_INTERACTION_HANDLING}${interaction.id}`)
+							.setPlaceholder("Select a bounty to take down...")
+							.setMaxValues(1)
+							.setOptions(bountyOptions)
+					)
+				]
+			})
+		}).then(reply => {
+			const collector = reply.createMessageComponentCollector({ max: 1 });
+			collector.on("collect", async (collectedInteraction) => {
+				const [bountyId] = collectedInteraction.values;
+				const bounty = await database.models.Bounty.findByPk(bountyId, { include: database.models.Bounty.Company });
+				bounty.state = "deleted";
+				bounty.save();
+				database.models.Completion.destroy({ where: { bountyId: bounty.id } });
+				if (bounty.Company.bountyBoardId) {
+					const bountyBoard = await interaction.guild.channels.fetch(bounty.Company.bountyBoardId);
+					const postingThread = await bountyBoard.threads.fetch(bounty.postingId);
+					postingThread.delete("Bounty taken down by poster");
+				}
+				bounty.destroy();
+
+				database.models.Hunter.findOne({ where: { userId: interaction.user.id, companyId: interaction.guildId } }).then(async hunter => {
+					hunter.decrement("xp");
+					const [season] = await database.models.Season.findOrCreate({ where: { companyId: interaction.guildId, isCurrentSeason: true } });
+					const [particiaption, participationCreated] = await database.models.Participation.findOrCreate({ where: { userId: interaction.user.id, companyId: interaction.guildId, seasonId: season.id }, defaults: { xp: -1 } });
+					if (!participationCreated) {
+						particiaption.decrement("xp");
+					}
+					getRankUpdates(interaction.guild, database);
+				})
+
+				collectedInteraction.reply({ content: "Your bounty has been taken down.", ephemeral: true });
+			})
+
+			collector.on("end", () => {
+				interaction.deleteReply();
+			})
+		})
 	})
 };
 
--- source/selects/_selectDictionary.js
+++ source/selects/_selectDictionary.js
@@ -6,7 +6,6 @@ const selectDictionary = {};
 for (const file of [
 	"bountyswapbounty.js",
 	"bountyswapslot.js",
-	"bountytakedown.js",
 	"evergreenedit.js",
 	"evergreenshowcase.js",
 	"evergreenswapbounty.js",
