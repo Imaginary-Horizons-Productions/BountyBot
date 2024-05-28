dc75a91119ba0657649e5815449766b856345017
Nathaniel Tseng <nathanieltseng@gmail.com>
2024-05-28 08:54:33 -0700

    add empty inventory text

--- source/commands/inventory.js
+++ source/commands/inventory.js
@@ -8,7 +8,12 @@ const mainId = "inventory";
 module.exports = new CommandWrapper(mainId, "Show the user the items in their inventory", PermissionFlagsBits.ViewChannel, false, true, 3000,
 	(interaction, database, runMode) => {
 		database.models.Item.findAll({ where: { userId: interaction.user.id, count: { [Op.gt]: 0 } } }).then(itemRows => {
-			const content = `Here are the items in your inventory (use them with ${commandMention("item")}):\n- ${itemRows.map(row => `${row.itemName} x ${row.count}`).join("\n- ")}`
+			let content = `Here are the items in your inventory (use them with ${commandMention("item")}):\n- `;
+			if (itemRows.length > 0) {
+				content += `${itemRows.map(row => `${row.itemName} x ${row.count}`).join("\n- ")}`;
+			} else {
+				content += "(None yet, do some bounties to find some!)";
+			}
 			if (content.length < MAX_MESSAGE_CONTENT_LENGTH) {
 				interaction.reply({ content, ephemeral: true });
 			} else {
