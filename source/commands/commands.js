3eca7cbc0ef0323aa3c50c0133b52863f16a5931
Nathaniel Tseng <nathanieltseng@gmail.com>
2024-10-14 22:30:42 -0700

    test commands page generation

--- source/commands/commands.js
+++ source/commands/commands.js
@@ -2,7 +2,7 @@ const { InteractionContextType } = require('discord.js');
 const { CommandWrapper } = require('../classes');
 
 const mainId = "commands";
-module.exports = new CommandWrapper(mainId, "Get a link to BountyBot's commands page", null, false, [InteractionContextType.BotDM, InteractionContextType.Guild, InteractionContextType.PrivateChannel], 3000,
+module.exports = new CommandWrapper(mainId, "Get a link to BountyBot's commands wiki", null, false, [InteractionContextType.BotDM, InteractionContextType.Guild, InteractionContextType.PrivateChannel], 3000,
 	/** Link the user to the repo Commands wiki page (automatically updated) */
 	(interaction, database, runMode) => {
 		interaction.reply({ content: "Here's a [link to the BountyBot Commands page](<https://github.com/Imaginary-Horizons-Productions/BountyBot/wiki/Commands>).", ephemeral: true });
