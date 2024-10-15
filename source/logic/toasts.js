62eb711fcce8fbc95c54338cd7f5689426277b2c
Nathaniel Tseng <nathanieltseng@gmail.com>
2024-10-15 11:37:00 -0700

    #263: improve crit toast formula documentation

--- source/logic/toasts.js
+++ source/logic/toasts.js
@@ -82,7 +82,16 @@ async function raiseToast(interaction, database, toasteeIds, toastText, imageURL
 					}
 				};
 
-				// f(x) = 150/(x+2)^(1/3)
+				/* f(x) > 150/(x+2)^(1/3)
+				where:
+				  f(x) = critRoll
+				  x + 2 = effectiveToastLevel
+				  150^3 = 3375000
+
+				notes:
+				- cubing both sides of the equation avoids the third root operation and prebakes the constant exponentiation
+				- constants set arbitrarily by user experience design
+				*/
 				if (critRoll * critRoll * critRoll > 3375000 / effectiveToastLevel) {
 					rawToast.wasCrit = true;
 					critValue += 1;
