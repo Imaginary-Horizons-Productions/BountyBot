27824ec6d3dea28c1d0e7f0868c08edadc71738b
Nathaniel Tseng <nathanieltseng@gmail.com>
2025-02-13 22:45:48 -0800

    clean out old migrations, convert migrations from current patch to new pattern

--- source/scripts/migrations/v2.8.0/migration3.js
+++
@@ -1,7 +0,0 @@
-const { DataTypes } = require("sequelize");
-const { connectToDatabase } = require("../../../../database.js");
-
-connectToDatabase("migration").then(async database => {
-	const queryInterface = database.getQueryInterface();
-	queryInterface.addColumn("Participation", "postingsCompleted", { type: DataTypes.INTEGER, defaultValue: 0 });
-});
--- source/scripts/migrations/v2.8.0/migration4.js
+++
@@ -1,7 +0,0 @@
-const { DataTypes } = require("sequelize");
-const { connectToDatabase } = require("../../../../database.js");
-
-connectToDatabase("migration").then(async database => {
-	const queryInterface = database.getQueryInterface();
-	queryInterface.addColumn("Participation", "toastsRaised", { type: DataTypes.INTEGER, defaultValue: 0 });
-});
