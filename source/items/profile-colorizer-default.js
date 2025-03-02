1d6c89c7a77b2d0640d97794a982670426425d07
Nathaniel Tseng <nathanieltseng@gmail.com>
2025-03-01 17:54:57 -0800

    #336: create `logicLayer.hunters.setHunterProfileColor()` (#338)
    - fix name error on Dark Purple Profile Colorizer

--- source/items/profile-colorizer-default.js
+++ source/items/profile-colorizer-default.js
@@ -9,10 +9,7 @@ const itemName = `${color} Profile Colorizer`;
 module.exports = new Item(itemName, "Changes the color of your stats profile embed to black", 3000,
 	/** Sets the user's Hunter profile to Colors.Default in the used guild */
 	async (interaction, database) => {
-		logicLayer.hunters.findOneHunter(interaction.user.id, interaction.guild.id).then(hunter => {
-			hunter.profileColor = color;
-			hunter.save();
-		})
+		logicLayer.hunters.setHunterProfileColor(interaction.user.id, interaction.guild.id, color);
 		interaction.reply({ content: `Your profile color has been set to black in this server.`, flags: [MessageFlags.Ephemeral] });
 	}
 ).setLogicLinker(logicBlob => {
