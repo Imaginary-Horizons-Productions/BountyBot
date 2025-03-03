d2b59e8c113d3e4d5bc32c5b5360ac02ee8c2e21
Nathaniel Tseng <nathanieltseng@gmail.com>
2025-03-03 14:06:38 -0800

    create endpoint `grantItem()`

--- source/items/unidentified-item.js
+++ source/items/unidentified-item.js
@@ -10,10 +10,7 @@ const itemName = "Unidentified Item";
 module.exports = new Item(itemName, "Rolls as a random item!", 3000,
 	async (interaction, database) => {
 		const rolledItem = rollItemDrop(1);
-		const [itemRow, itemWasCreated] = await database.models.Item.findOrCreate({ where: { userId: interaction.user.id, itemName: rolledItem } });
-		if (!itemWasCreated) {
-			itemRow.increment("count");
-		}
+		logicLayer.items.grantItem(interaction.user.id, rolledItem);
 		interaction.reply({ content: `The unidentified item was a **${rolledItem}**! Use it with ${commandMention("item")}?`, flags: [MessageFlags.Ephemeral] });
 	}
 ).setLogicLinker(logicBlob => {
--- source/logic/items.js
+++ source/logic/items.js
@@ -15,7 +15,20 @@ function getInventory(userId) {
 	return db.models.Item.findAll({ where: { userId, count: { [Op.gt]: 0 } } });
 }
 
+/** *Grants the User 1 copy of the specified Item*
+ * @param {string} userId
+ * @param {string} itemName
+ */
+function grantItem(userId, itemName) {
+	db.models.Item.findOrCreate({ where: { userId, itemName } }).then(([itemRow, itemWasCreated]) => {
+		if (!itemWasCreated) {
+			itemRow.increment("count");
+		}
+	});
+}
+
 module.exports = {
 	setDB,
-	getInventory
+	getInventory,
+	grantItem
 }
