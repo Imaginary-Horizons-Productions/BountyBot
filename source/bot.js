28676cbe07bd8bd8b0ef6e2a1b21c3a5a4e8cedf
SillySalamndr <l.ensign@hotmail.com>
2025-03-05 00:34:41 -0500

    Suggestion to decouple the event callback setup from the database setup.
    We're able to capture the database being ready in a promise variable, which we can then `await` on the specific node callbacks that actually care about the database/logic files (which is admittedly _most of them_, but being able to do this decoupling for the ClientReady is a bit of a performance boost in being able to go through the motions of getting logged in and posting basic update information while the database is doing some transactions. We also avoid a pitfall of implying that the final `catch` is able to handle the errors produced within the callback functions set on the client events. That `catch` would be able to handle any errors that happen as those callbacks are set on those events, but once those callbacks are set, the error handling is within a different calling context (but not variable/block context) within the Node session.

--- source/bot.js
+++ source/bot.js
@@ -46,8 +46,8 @@ const client = new Client({
 const interactionCooldowns = new Map();
 //#endregion
 
-//#region Executing Code
-sequelize.authenticate().then(async () => {
+//#region Database Setup
+const dbReady = sequelize.authenticate().then(async () => {
 	return (await fsa.readdir(path.join(__dirname, "models"))).filter(directory => directory.indexOf('.') === -1);
 }).then(directories => {
 	return Promise.all(directories.map(async directory => {
@@ -69,188 +69,195 @@ sequelize.authenticate().then(async () => {
 			models[modelName].associate(models);
 		}
 	});
-}).then(() => {
 	for (logicFile in logicBlob) { // Set the database for the logic files that store it
 		logicBlob[logicFile].setDB?.(sequelize);
 	}
+	
 	setCommandLogic(logicBlob);
 	setButtonLogic(logicBlob);
 	setSelectLogic(logicBlob);
 	setContextMenuLogic(logicBlob);
 	setItemLogic(logicBlob);
 
-	client.login(require(authPath).token);
+	return;
+}).catch(console.error);
+//#endregion
 
-	//#region Event Handlers
-	client.on(Events.ClientReady, () => {
-		console.log(`Connected as ${client.user.tag}`);
-		if (runMode === "production") {
-			(() => {
-				try {
-					new REST({ version: 10 }).setToken(require(authPath).token).put(
-						Routes.applicationCommands(client.user.id),
-						{ body: [...slashData, ...contextMenuData] }
-					).then(commands => {
-						for (const command of commands) {
-							commandIds[command.name] = command.id;
-						}
-					})
-				} catch (error) {
-					console.error(error);
-				}
-			})()
+client.login(require(authPath).token);
 
-			// Post Changelog
-			if (announcementsChannelId) {
-				fsa.readFile('./ChangeLog.md', { encoding: 'utf8' }).then(data => {
-					let [currentFull, currentMajor, currentMinor, currentPatch] = data.match(/(\d+)\.(\d+)\.(\d+)/);
-					let [_lastFull, lastMajor, lastMinor, lastPatch] = lastPostedVersion.match(/(\d+)\.(\d+)\.(\d+)/);
-
-					if (parseInt(currentMajor) <= parseInt(lastMajor) && parseInt(currentMinor) <= parseInt(lastMinor) && parseInt(currentPatch) <= parseInt(lastPatch)) {
-						return;
+//#region Event Handlers
+client.on(Events.ClientReady, () => {
+	console.log(`Connected as ${client.user.tag}`);
+	if (runMode === "production") {
+		(() => {
+			try {
+				new REST({ version: 10 }).setToken(require(authPath).token).put(
+					Routes.applicationCommands(client.user.id),
+					{ body: [...slashData, ...contextMenuData] }
+				).then(commands => {
+					for (const command of commands) {
+						commandIds[command.name] = command.id;
 					}
-
-					buildVersionEmbed(client.user.displayAvatarURL()).then(embed => {
-						client.guilds.fetch(testGuildId).then(guild => {
-							guild.channels.fetch(announcementsChannelId).then(announcementsChannel => {
-								announcementsChannel.send({ embeds: [embed] }).then(message => {
-									message.crosspost();
-									fsa.writeFile('./config/versionData.json', JSON.stringify({
-										announcementsChannelId,
-										lastPostedVersion: currentFull,
-									}), "utf-8");
-								});
-							})
-						})
-					}).catch(console.error);
-				});
-			} else {
-				console.error("Patch notes post skipped due to falsy announcementsChannelId");
-			}
-		} else {
-			client.application.commands.fetch({ guildId: testGuildId }).then(commands => {
-				commands.each(command => {
-					commandIds[command.name] = command.id;
 				})
+			} catch (error) {
+				console.error(error);
+			}
+		})()
+
+		// Post Changelog
+		if (announcementsChannelId) {
+			fsa.readFile('./ChangeLog.md', { encoding: 'utf8' }).then(data => {
+				let [currentFull, currentMajor, currentMinor, currentPatch] = data.match(/(\d+)\.(\d+)\.(\d+)/);
+				let [_lastFull, lastMajor, lastMinor, lastPatch] = lastPostedVersion.match(/(\d+)\.(\d+)\.(\d+)/);
+
+				if (parseInt(currentMajor) <= parseInt(lastMajor) && parseInt(currentMinor) <= parseInt(lastMinor) && parseInt(currentPatch) <= parseInt(lastPatch)) {
+					return;
+				}
+
+				buildVersionEmbed(client.user.displayAvatarURL()).then(embed => {
+					client.guilds.fetch(testGuildId).then(guild => {
+						guild.channels.fetch(announcementsChannelId).then(announcementsChannel => {
+							announcementsChannel.send({ embeds: [embed] }).then(message => {
+								message.crosspost();
+								fsa.writeFile('./config/versionData.json', JSON.stringify({
+									announcementsChannelId,
+									lastPostedVersion: currentFull,
+								}), "utf-8");
+							});
+						})
+					})
+				}).catch(console.error);
 			});
-		}
-	});
-
-	client.on(Events.InteractionCreate, interaction => {
-		if (interaction.isAutocomplete()) {
-			const command = getCommand(interaction.commandName);
-			const focusedOption = interaction.options.getFocused(true);
-			const unfilteredChoices = command.autocomplete?.[focusedOption.name] ?? [];
-			if (unfilteredChoices.length < 1) {
-				console.error(`Attempted autocomplete on misconfigured command ${interaction.commandName} ${focusedOption.name}`);
-			}
-			const choices = unfilteredChoices.filter(choice => choice.value.toLowerCase().includes(focusedOption.value.toLowerCase()))
-				.slice(0, 25);
-			interaction.respond(choices);
-		} else if (interaction.isContextMenuCommand()) {
-			const contextMenu = getContextMenu(interaction.commandName);
-			if (contextMenu.premiumCommand && !premium.paid.includes(interaction.user.id) && !premium.gift.includes(interaction.user.id)) {
-				interaction.reply({ content: `The \`/${interaction.commandName}\` context menu option is a premium command. Learn more with ${commandMention("premium")}.`, flags: [MessageFlags.Ephemeral] });
-				return;
-			}
-
-			const cooldownTimestamp = contextMenu.getCooldownTimestamp(interaction.user.id, interactionCooldowns);
-			if (cooldownTimestamp) {
-				interaction.reply({ content: `Please wait, the \`/${interaction.commandName}\` context menu option is on cooldown. It can be used again <t:${cooldownTimestamp}:R>.`, flags: [MessageFlags.Ephemeral] });
-				return;
-			}
-
-			contextMenu.execute(interaction, sequelize, runMode);
-		} else if (interaction.isCommand()) {
-			const command = getCommand(interaction.commandName);
-			if (command.premiumCommand && !premium.paid.includes(interaction.user.id) && !premium.gift.includes(interaction.user.id)) {
-				interaction.reply({ content: `The \`/${interaction.commandName}\` command is a premium command. Learn more with ${commandMention("premium")}.`, flags: [MessageFlags.Ephemeral] });
-				return;
-			}
-
-			const cooldownTimestamp = command.getCooldownTimestamp(interaction.user.id, interactionCooldowns);
-			if (cooldownTimestamp) {
-				interaction.reply({ content: `Please wait, the \`/${interaction.commandName}\` command is on cooldown. It can be used again <t:${cooldownTimestamp}:R>.`, flags: [MessageFlags.Ephemeral] });
-				return;
-			}
-			command.execute(interaction, sequelize, runMode);
-		} else if (interaction.customId.startsWith(SKIP_INTERACTION_HANDLING)) {
-			return;
 		} else {
-			const [mainId, ...args] = interaction.customId.split(SAFE_DELIMITER);
-			let getter;
-			if (interaction.isButton()) {
-				getter = getButton;
-			} else if (interaction.isAnySelectMenu()) {
-				getter = getSelect;
-			}
-			const interactionWrapper = getter(mainId);
-			const cooldownTimestamp = interactionWrapper.getCooldownTimestamp(interaction.user.id, interactionCooldowns);
-
-			if (cooldownTimestamp) {
-				interaction.reply({ content: `Please wait, this interaction is on cooldown. It can be used again <t:${cooldownTimestamp}:R>.`, flags: [MessageFlags.Ephemeral] });
-				return;
-			}
-
-			interactionWrapper.execute(interaction, args, sequelize, runMode);
+			console.error("Patch notes post skipped due to falsy announcementsChannelId");
 		}
-	});
-
-	client.on(Events.ChannelDelete, channel => {
-		logicBlob.companies.findCompanyByPK(channel.guild.id).then(company => {
-			if (company) {
-				let shouldSaveCompany = false;
-				if (channel.id === company.bountyBoardId) {
-					company.bountyBoardId = null;
-					shouldSaveCompany = true;
-				} else if (channel.id === company.scoreboardChannelId) {
-					company.scoreboardChannelId = null;
-					shouldSaveCompany = true;
-				}
-				if (shouldSaveCompany) {
-					company.save();
-				}
-			}
-		})
-	});
-
-	client.on(Events.MessageDelete, message => {
-		logicBlob.companies.findCompanyByPK(message.guild.id).then(company => {
-			if (message.id === company.scoreboardMessageId) {
-				company.scoreboardMessageId = null;
-				company.save();
-			}
-		})
-	});
-
-	client.on(Events.ThreadDelete, thread => {
-		logicBlob.companies.findCompanyByPK(thread.guild.id).then(company => {
-			if (thread.id === company.evergreenThreadId) {
-				company.evergreenThreadId = null;
-				company.save();
-			}
-		})
-	})
-
-	client.on(Events.GuildDelete, guild => {
-		sequelize.models.Hunter.destroy({ where: { companyId: guild.id } });
-		sequelize.models.Toast.findAll({ where: { companyId: guild.id } }).then(toasts => {
-			toasts.forEach(toast => {
-				sequelize.models.Recipient.destroy({ where: { toastId: toast.id } });
-				sequelize.models.Seconding.destroy({ where: { toastId: toast.id } });
-				toast.destroy();
+	} else {
+		client.application.commands.fetch({ guildId: testGuildId }).then(commands => {
+			commands.each(command => {
+				commandIds[command.name] = command.id;
 			})
 		});
+	}
+});
 
-		sequelize.models.Bounty.destroy({ where: { companyId: guild.id } });
-		sequelize.models.Completion.destroy({ where: { companyId: guild.id } });
+client.on(Events.InteractionCreate, async interaction => {
+	await dbReady;
+	if (interaction.isAutocomplete()) {
+		const command = getCommand(interaction.commandName);
+		const focusedOption = interaction.options.getFocused(true);
+		const unfilteredChoices = command.autocomplete?.[focusedOption.name] ?? [];
+		if (unfilteredChoices.length < 1) {
+			console.error(`Attempted autocomplete on misconfigured command ${interaction.commandName} ${focusedOption.name}`);
+		}
+		const choices = unfilteredChoices.filter(choice => choice.value.toLowerCase().includes(focusedOption.value.toLowerCase()))
+			.slice(0, 25);
+		interaction.respond(choices);
+	} else if (interaction.isContextMenuCommand()) {
+		const contextMenu = getContextMenu(interaction.commandName);
+		if (contextMenu.premiumCommand && !premium.paid.includes(interaction.user.id) && !premium.gift.includes(interaction.user.id)) {
+			interaction.reply({ content: `The \`/${interaction.commandName}\` context menu option is a premium command. Learn more with ${commandMention("premium")}.`, flags: [MessageFlags.Ephemeral] });
+			return;
+		}
 
-		sequelize.models.Participation.destroy({ where: { companyId: guild.id } });
-		logicBlob.seasons.deleteCompanySeasons(guild.id);
+		const cooldownTimestamp = contextMenu.getCooldownTimestamp(interaction.user.id, interactionCooldowns);
+		if (cooldownTimestamp) {
+			interaction.reply({ content: `Please wait, the \`/${interaction.commandName}\` context menu option is on cooldown. It can be used again <t:${cooldownTimestamp}:R>.`, flags: [MessageFlags.Ephemeral] });
+			return;
+		}
 
-		logicBlob.ranks.deleteRanks(guild.id);
-		sequelize.models.Company.destroy({ where: { id: guild.id } });
+		contextMenu.execute(interaction, sequelize, runMode);
+	} else if (interaction.isCommand()) {
+		const command = getCommand(interaction.commandName);
+		if (command.premiumCommand && !premium.paid.includes(interaction.user.id) && !premium.gift.includes(interaction.user.id)) {
+			interaction.reply({ content: `The \`/${interaction.commandName}\` command is a premium command. Learn more with ${commandMention("premium")}.`, flags: [MessageFlags.Ephemeral] });
+			return;
+		}
+
+		const cooldownTimestamp = command.getCooldownTimestamp(interaction.user.id, interactionCooldowns);
+		if (cooldownTimestamp) {
+			interaction.reply({ content: `Please wait, the \`/${interaction.commandName}\` command is on cooldown. It can be used again <t:${cooldownTimestamp}:R>.`, flags: [MessageFlags.Ephemeral] });
+			return;
+		}
+		command.execute(interaction, sequelize, runMode);
+	} else if (interaction.customId.startsWith(SKIP_INTERACTION_HANDLING)) {
+		return;
+	} else {
+		const [mainId, ...args] = interaction.customId.split(SAFE_DELIMITER);
+		let getter;
+		if (interaction.isButton()) {
+			getter = getButton;
+		} else if (interaction.isAnySelectMenu()) {
+			getter = getSelect;
+		}
+		const interactionWrapper = getter(mainId);
+		const cooldownTimestamp = interactionWrapper.getCooldownTimestamp(interaction.user.id, interactionCooldowns);
+
+		if (cooldownTimestamp) {
+			interaction.reply({ content: `Please wait, this interaction is on cooldown. It can be used again <t:${cooldownTimestamp}:R>.`, flags: [MessageFlags.Ephemeral] });
+			return;
+		}
+
+		interactionWrapper.execute(interaction, args, sequelize, runMode);
+	}
+});
+
+client.on(Events.ChannelDelete, async channel => {
+	await dbReady;
+	logicBlob.companies.findCompanyByPK(channel.guild.id).then(company => {
+		if (company) {
+			let shouldSaveCompany = false;
+			if (channel.id === company.bountyBoardId) {
+				company.bountyBoardId = null;
+				shouldSaveCompany = true;
+			} else if (channel.id === company.scoreboardChannelId) {
+				company.scoreboardChannelId = null;
+				shouldSaveCompany = true;
+			}
+			if (shouldSaveCompany) {
+				company.save();
+			}
+		}
+	})
+});
+
+client.on(Events.MessageDelete, async message => {
+	await dbReady;
+	logicBlob.companies.findCompanyByPK(message.guild.id).then(company => {
+		if (message.id === company.scoreboardMessageId) {
+			company.scoreboardMessageId = null;
+			company.save();
+		}
+	})
+});
+
+client.on(Events.ThreadDelete, async thread => {
+	await dbReady;
+	logicBlob.companies.findCompanyByPK(thread.guild.id).then(company => {
+		if (thread.id === company.evergreenThreadId) {
+			company.evergreenThreadId = null;
+			company.save();
+		}
+	})
+})
+
+client.on(Events.GuildDelete, async guild => {
+	await dbReady;
+	sequelize.models.Hunter.destroy({ where: { companyId: guild.id } });
+	sequelize.models.Toast.findAll({ where: { companyId: guild.id } }).then(toasts => {
+		toasts.forEach(toast => {
+			sequelize.models.Recipient.destroy({ where: { toastId: toast.id } });
+			sequelize.models.Seconding.destroy({ where: { toastId: toast.id } });
+			toast.destroy();
+		})
 	});
-	//#endregion
-}).catch(console.error);
+
+	sequelize.models.Bounty.destroy({ where: { companyId: guild.id } });
+	sequelize.models.Completion.destroy({ where: { companyId: guild.id } });
+
+	sequelize.models.Participation.destroy({ where: { companyId: guild.id } });
+	logicBlob.seasons.deleteCompanySeasons(guild.id);
+
+	logicBlob.ranks.deleteRanks(guild.id);
+	sequelize.models.Company.destroy({ where: { id: guild.id } });
+});
 //#endregion
