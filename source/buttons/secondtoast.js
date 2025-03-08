12711bb9433ddee20cb106a3bc239ed5691fb64b
ntseng <nathanieltseng@gmail.com>
2025-03-08 18:40:47 +0000

    Deploying to main from @ Imaginary-Horizons-Productions/BountyBot@4e8de74b1df520d3e283e68ba288b7b45551a92a 🚀

--- source/buttons/secondtoast.js
+++ source/buttons/secondtoast.js
@@ -1,8 +1,7 @@
 const { EmbedBuilder, MessageFlags } = require('discord.js');
 const { ButtonWrapper } = require('../classes');
-const { Op } = require('sequelize');
 const { getRankUpdates } = require('../util/scoreUtil');
-const { timeConversion, generateTextBar } = require('../util/textUtil');
+const { generateTextBar } = require('../util/textUtil');
 const { updateScoreboard } = require('../util/embedUtil');
 const { Seconding } = require('../models/toasts/Seconding');
 const { Goal } = require('../models/companies/Goal');
@@ -54,7 +53,7 @@ module.exports = new ButtonWrapper(mainId, 3000,
 			hunter.increment("toastsReceived");
 		}
 
-		const recentToasts = await database.models.Seconding.findAll({ where: { seconderId: interaction.user.id, createdAt: { [Op.gt]: new Date(new Date() - 2 * timeConversion(1, "d", "ms")) } } });
+		const recentToasts = await logicLayer.toasts.findRecentSecondings(interaction.user.id);
 		let critSecondsAvailable = 2;
 		for (const seconding of recentToasts) {
 			if (seconding.wasCrit) {
