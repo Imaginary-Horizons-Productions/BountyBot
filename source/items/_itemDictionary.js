a06412f60a40936fae21717033913b9cda9ff340
Nathaniel Tseng <nathanieltseng@gmail.com>
2025-03-17 12:15:54 -0700

    #345: consolidate Colorizer items

--- source/items/_itemDictionary.js
+++ source/items/_itemDictionary.js
@@ -9,47 +9,21 @@ const ITEM_NAMES = [];
 for (const file of [
 	"bonus-bounty-showcase.js",
 	"bounty-thumbnail.js",
+	"colorizers.js",
 	"goal-initializer.js",
 	"loot-box.js",
-	"profile-colorizer-aqua.js",
-	"profile-colorizer-blue.js",
-	"profile-colorizer-blurple.js",
-	"profile-colorizer-darkaqua.js",
-	"profile-colorizer-darkblue.js",
-	"profile-colorizer-darkbutnotblack.js",
-	"profile-colorizer-darkergrey.js",
-	"profile-colorizer-darkgold.js",
-	"profile-colorizer-darkgreen.js",
-	"profile-colorizer-darkgrey.js",
-	"profile-colorizer-darknavy.js",
-	"profile-colorizer-darkorange.js",
-	"profile-colorizer-darkpurple.js",
-	"profile-colorizer-darkred.js",
-	"profile-colorizer-darkvividpink.js",
-	"profile-colorizer-default.js",
-	"profile-colorizer-fuchsia.js",
-	"profile-colorizer-gold.js",
-	"profile-colorizer-green.js",
-	"profile-colorizer-grey.js",
-	"profile-colorizer-lightgrey.js",
-	"profile-colorizer-luminousvividpink.js",
-	"profile-colorizer-navy.js",
-	"profile-colorizer-notquiteblack.js",
-	"profile-colorizer-orange.js",
-	"profile-colorizer-purple.js",
-	"profile-colorizer-red.js",
-	"profile-colorizer-white.js",
-	"profile-colorizer-yellow.js",
 	"progress-in-a-can.js",
 	"unidentified-item.js",
 	"xp-boost-epic.js",
 	"xp-boost-legendary.js",
 	"xp-boost.js"
 ]) {
-	/** @type {ItemTemplate} */
-	const item = require(`./${file}`);
-	ITEMS[item.name] = item;
-	ITEM_NAMES.push(item.name);
+	/** @type {ItemTemplate[]} */
+	const fileItems = [require(`./${file}`)].flat();
+	for (const item of fileItems) {
+		ITEMS[item.name] = item;
+		ITEM_NAMES.push(item.name);
+	}
 }
 
 /** @param {string[]} exclusions */
