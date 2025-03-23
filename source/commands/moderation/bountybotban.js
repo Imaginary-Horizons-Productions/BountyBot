7fce4197d8f5607f6633007c50b7c6bc244133a2
SillySalamndr <l.ensign@hotmail.com>
2025-03-22 21:47:20 -0400

    Fixed incorrect bot reference (updated variable name) Fixed bad userId reference

--- source/commands/moderation/bountybotban.js
+++ source/commands/moderation/bountybotban.js
@@ -3,17 +3,17 @@ const { SubcommandWrapper } = require("../../classes");
 
 module.exports = new SubcommandWrapper("bountybot-ban", "Toggle whether the provided user can interact with bounties or toasts",
 	async function executeSubcommand(interaction, runMode, ...[logicLayer]) {
-		const member = interaction.options.getUser("user");
+		const discordUser = interaction.options.getUser("user");
 		await logicLayer.companies.findOrCreateCompany(interaction.guild.id);
-		const [hunter] = await logicLayer.hunters.findOrCreateBountyHunter(member.id, interaction.guild.id);
+		const [hunter] = await logicLayer.hunters.findOrCreateBountyHunter(discordUser.id, interaction.guild.id);
 		hunter.isBanned = !hunter.isBanned;
 		if (hunter.isBanned) {
 			hunter.hasBeenBanned = true;
 		}
 		hunter.save();
-		interaction.reply({ content: `${member} has been ${hunter.isBanned ? "" : "un"}banned from interacting with BountyBot.`, flags: [MessageFlags.Ephemeral] });
-		if (!member.user.bot) {
-			member.send(`You have been ${hunter.isBanned ? "" : "un"}banned from interacting with BountyBot on ${interaction.guild.name}. The reason provided was: ${interaction.options.getString("reason")}`);
+		interaction.reply({ content: `${discordUser} has been ${hunter.isBanned ? "" : "un"}banned from interacting with BountyBot.`, flags: [MessageFlags.Ephemeral] });
+		if (!discordUser.bot) {
+			discordUser.send(`You have been ${hunter.isBanned ? "" : "un"}banned from interacting with BountyBot on ${interaction.guild.name}. The reason provided was: ${interaction.options.getString("reason")}`);
 		}
 	}
 ).setOptions(
