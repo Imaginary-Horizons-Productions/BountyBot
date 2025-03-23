3f5f4e94b6cd9574f0871282d51774e03985175c
SillySalamndr <l.ensign@hotmail.com>
2025-03-22 22:09:37 -0400

    Fixed scoreboard delete issue

--- source/bot.js
+++ source/bot.js
@@ -42,7 +42,7 @@ const dAPIClient = new Client({
 			name: "🔰 Get started with /tutorial"
 		}]
 	},
-	intents: [IntentsBitField.Flags.Guilds, IntentsBitField.Flags.GuildMembers]
+	intents: [IntentsBitField.Flags.Guilds, IntentsBitField.Flags.GuildMembers, IntentsBitField.Flags.GuildMessages]
 });
 /** @type {Map<string, Map<string, number>>} */
 const interactionCooldowns = new Map();
