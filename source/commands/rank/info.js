97bfb27305331b6b0e942883a55122b48083221a
SillySalamndr <l.ensign@hotmail.com>
2025-04-03 23:28:47 -0400

    Refactor `/rank info` to show all ranks Fixes #448

--- source/commands/rank/info.js
+++ source/commands/rank/info.js
@@ -1,28 +1,26 @@
 const { MessageFlags } = require("discord.js");
+const { Buffer } = require('node:buffer');
 const { SubcommandWrapper } = require("../../classes");
+const { MAX_MESSAGE_CONTENT_LENGTH } = require("../../constants");
 
 module.exports = new SubcommandWrapper("info", "Get the information about an existing seasonal rank",
 	async function executeSubcommand(interaction, runMode, ...[logicLayer]) {
-		const varianceThreshold = interaction.options.getNumber("variance-threshold");
 		const ranks = await logicLayer.ranks.findAllRanks(interaction.guild.id);
-		let index = 0;
-		const rank = ranks.find(rank => {
-			index++;
-			return rank.varianceThreshold == varianceThreshold
-		});
-
-		if (!rank) {
-			interaction.reply({ content: `Could not find a seasonal rank with variance threshold of ${varianceThreshold}.`, flags: [MessageFlags.Ephemeral] });
+		if (!ranks || !ranks.length) {
+			interaction.reply({ content: `Could not find aany seasonal ranks. Please contact a server admin to make sure this isn't a mistake.`, flags: [MessageFlags.Ephemeral] });
 			return;
 		}
+		const rankStrings = ranks.map((rank, index) => {
+			return `${rank.rankmoji ?? ""}${rank.roleId ? `<@&${rank.roleId}>` : `Rank ${index}`}\nVariance Threshold: ${rank.varianceThreshold}`;
+		}).join('\n');
 
-		interaction.reply({ content: `${rank.rankmoji ?? ""}${rank.roleId ? `<@&${rank.roleId}>` : `Rank ${index}`}\nVariance Threshold: ${rank.varianceThreshold}`, flags: [MessageFlags.Ephemeral] });
-	}
-).setOptions(
-	{
-		type: "Number",
-		name: "variance-threshold",
-		description: "The variance threshold of the rank to view",
-		required: true
+		const msgJson = { flags: [MessageFlags.Ephemeral] };
+		if (rankStrings.length > MAX_MESSAGE_CONTENT_LENGTH) { // Send large content as a text file
+			msgJson.files = [ Buffer.from(rankStrings, 'utf16le') ];
+		} else {
+			msgJson.content = rankStrings;
+		}
+
+		interaction.reply(msgJson);
 	}
 );
