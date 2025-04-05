7082433825551e6aaa8c9c41a2b6396a22129743
SillySalamndr <l.ensign@hotmail.com>
2025-04-05 22:27:52 +0000

    #448: show all ranks in `/rank info` instead of querying user (#453)
    * Refactor `/rank info` to show all ranks
    Fixes #448
    
    * Minor fixes pre-test
    
    * Updated variable name
    Fixed a bug in rank roles where applying the defaults would hit a missing function
    
    ---------
    
    Co-authored-by: Nathaniel Tseng <nathanieltseng@gmail.com>

--- source/commands/rank/info.js
+++ source/commands/rank/info.js
@@ -1,28 +1,25 @@
 const { MessageFlags } = require("discord.js");
 const { SubcommandWrapper } = require("../../classes");
+const { MAX_MESSAGE_CONTENT_LENGTH } = require("../../constants");
 
 module.exports = new SubcommandWrapper("info", "Get the information about an existing seasonal rank",
 	async function executeSubcommand(interaction, runMode, ...[logicLayer]) {
-		const varianceThreshold = interaction.options.getNumber("variance-threshold");
 		const ranks = await logicLayer.ranks.findAllRanks(interaction.guild.id);
-		let index = 0;
-		const rank = ranks.find(rank => {
-			index++;
-			return rank.varianceThreshold == varianceThreshold
-		});
-
-		if (!rank) {
-			interaction.reply({ content: `Could not find a seasonal rank with variance threshold of ${varianceThreshold}.`, flags: [MessageFlags.Ephemeral] });
+		if (!ranks || !ranks.length) {
+			interaction.reply({ content: `Could not find aany seasonal ranks. Please contact a server admin to make sure this isn't a mistake.`, flags: [MessageFlags.Ephemeral] });
 			return;
 		}
+		const allRanksMsg = ranks.map((rank, index) => {
+			return `${rank.rankmoji ?? ""}${rank.roleId ? `<@&${rank.roleId}>` : `Rank ${index}`}\nVariance Threshold: ${rank.varianceThreshold}`;
+		}).join('\n');
 
-		interaction.reply({ content: `${rank.rankmoji ?? ""}${rank.roleId ? `<@&${rank.roleId}>` : `Rank ${index}`}\nVariance Threshold: ${rank.varianceThreshold}`, flags: [MessageFlags.Ephemeral] });
-	}
-).setOptions(
-	{
-		type: "Number",
-		name: "variance-threshold",
-		description: "The variance threshold of the rank to view",
-		required: true
+		const msgJson = { flags: [MessageFlags.Ephemeral] };
+		if (allRanksMsg.length < MAX_MESSAGE_CONTENT_LENGTH) { // Send large content as a text file
+			msgJson.content = allRanksMsg;
+		} else {
+			msgJson.files = [new AttachmentBuilder(Buffer.from(allRanksMsg, 'utf16le'), { name: 'ranks.txt' })];
+		}
+
+		interaction.reply(msgJson);
 	}
 );
