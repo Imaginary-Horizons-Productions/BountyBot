f4cf028be3d145461390cace294b6c2f33c6e361
Nathaniel Tseng <nathanieltseng@gmail.com>
2025-04-07 16:47:05 -0700

    update `updateScoreboard` interface on `/festival start` and `/festival close`

--- source/commands/festival/close.js
+++ source/commands/festival/close.js
@@ -7,6 +7,14 @@ module.exports = new SubcommandWrapper("close", "End the festival, returning to 
 			bountyBot.setNickname(null);
 		})
 		interaction.reply(company.sendAnnouncement({ content: "The XP multiplier festival has ended. Hope you participate next time!" }));
-		company.updateScoreboard(interaction.guild, logicLayer);
+		const [season] = await logicLayer.seasons.findOrCreateCurrentSeason(interaction.guild.id);
+		const embeds = [];
+		const ranks = await logicLayer.ranks.findAllRanks(interaction.guild.id);
+		if (company.scoreboardIsSeasonal) {
+			embeds.push(await company.seasonalScoreboardEmbed(interaction.guild, await logicLayer.seasons.findSeasonParticipations(season.id), ranks));
+		} else {
+			embeds.push(await company.overallScoreboardEmbed(interaction.guild, await logicLayer.hunters.findCompanyHunters(interaction.guild.id), ranks));
+		}
+		company.updateScoreboard(interaction.guild, embeds);
 	}
 );
