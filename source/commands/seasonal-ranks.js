5102760c899420a9a8fa6b403dcc2f1a76a87192
Nathaniel Tseng <nathanieltseng@gmail.com>
2025-04-08 14:42:08 -0700

    add explanation to roles list, general cleanup

--- source/commands/seasonal-ranks.js
+++ source/commands/seasonal-ranks.js
@@ -1,4 +1,4 @@
-const { PermissionFlagsBits, InteractionContextType, MessageFlags } = require('discord.js');
+const { PermissionFlagsBits, InteractionContextType, MessageFlags, roleMention, AttachmentBuilder, heading } = require('discord.js');
 const { CommandWrapper } = require('../classes/index.js');
 const { MAX_MESSAGE_CONTENT_LENGTH } = require("../constants.js");
 
@@ -10,21 +10,21 @@ module.exports = new CommandWrapper(mainId, "Look up this server's seasonal rank
 	async (interaction, runMode) => {
 		const ranks = await logicLayer.ranks.findAllRanks(interaction.guild.id);
 		if (!ranks || !ranks.length) {
-			interaction.reply({ content: `Could not find aany seasonal ranks. Please contact a server admin to make sure this isn't a mistake.`, flags: [MessageFlags.Ephemeral] });
+			interaction.reply({ content: `Could not find any seasonal ranks. Please contact a server admin to make sure this isn't a mistake.`, flags: [MessageFlags.Ephemeral] });
 			return;
 		}
-		const allRanksMsg = ranks.map((rank, index) => {
-			return `${rank.rankmoji ?? ""}${rank.roleId ? `<@&${rank.roleId}>` : `Rank ${index}`}\nVariance Threshold: ${rank.varianceThreshold}`;
-		}).join('\n');
+		const content = `${heading("Seasonal Ranks", 1)}\nBounty Hunters who earn more XP compared to their contemporaries are given special roles to distinguish themselves for the season. These roles are as follows:\n\n${ranks.map((rank, index) => {
+			return `${rank.rankmoji ?? ""}${rank.roleId ? roleMention(rank.roleId) : `Rank ${index}`}\nVariance Threshold: ${rank.varianceThreshold}\n`;
+		}).join('\n')}`;
 
-		const msgJson = { flags: [MessageFlags.Ephemeral] };
-		if (allRanksMsg.length < MAX_MESSAGE_CONTENT_LENGTH) { // Send large content as a text file
-			msgJson.content = allRanksMsg;
+		const messagePayload = { flags: [MessageFlags.Ephemeral] };
+		if (content.length < MAX_MESSAGE_CONTENT_LENGTH) { // Send large content as a text file
+			messagePayload.content = content;
 		} else {
-			msgJson.files = [new AttachmentBuilder(Buffer.from(allRanksMsg, 'utf16le'), { name: 'ranks.txt' })];
+			messagePayload.files = [new AttachmentBuilder(Buffer.from(content, 'utf16le'), { name: 'ranks.txt' })];
 		}
 
-		interaction.reply(msgJson);
+		interaction.reply(messagePayload);
 	}
 ).setLogicLinker(logicBlob => {
 	logicLayer = logicBlob;
