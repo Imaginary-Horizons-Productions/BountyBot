f35c9731ec12f063ec21d45cbc2e09b6fb5cc5cb
Nathaniel Tseng <nathanieltseng@gmail.com>
2025-04-14 10:42:59 -0700

    #468: switch `/bounty remove-completers` option from String to User

--- source/commands/bounty/removecompleters.js
+++ source/commands/bounty/removecompleters.js
@@ -1,5 +1,5 @@
 const { MessageFlags, userMention } = require("discord.js");
-const { extractUserIdsFromMentions, listifyEN } = require("../../util/textUtil");
+const { listifyEN } = require("../../util/textUtil");
 const { SubcommandWrapper } = require("../../classes");
 
 module.exports = new SubcommandWrapper("remove-completers", "Remove hunter(s) from a bounty's list of completers",
@@ -11,37 +11,63 @@ module.exports = new SubcommandWrapper("remove-completers", "Remove hunter(s) fr
 				return;
 			}
 
-			const mentionedIds = extractUserIdsFromMentions(interaction.options.getString("hunters"), []);
-			if (mentionedIds.length < 1) {
-				interaction.reply({ content: "Could not find any user mentions in `hunters`.", flags: [MessageFlags.Ephemeral] });
-				return;
+			const hunterIds = [];
+			for (const potentialHunter of ["bounty-hunter", "second-bounty-hunter", "third-bounty-hunter", "fourth-bounty-hunter", "fifth-bounty-hunter"]) {
+				const guildMember = interaction.options.getMember(potentialHunter);
+				if (guildMember) {
+					hunterIds.push(guildMember.id);
+				}
 			}
 
-			logicLayer.bounties.deleteSelectedBountyCompletions(bounty.id, mentionedIds);
+			logicLayer.bounties.deleteSelectedBountyCompletions(bounty.id, hunterIds);
 			const company = await logicLayer.companies.findCompanyByPK(interaction.guildId);
 			bounty.updatePosting(interaction.guild, company, (await logicLayer.hunters.findOneHunter(posterId, interaction.guild.id)).level, await logicLayer.bounties.findBountyCompletions(bounty.id));
 			if (company.bountyBoardId) {
 				interaction.guild.channels.fetch(company.bountyBoardId).then(bountyBoard => {
 					return bountyBoard.threads.fetch(bounty.postingId);
 				}).then(posting => {
-					posting.send({ content: `${listifyEN(mentionedIds.map(id => `<@${id}>`))} ${mentionedIds.length === 1 ? "has" : "have"} been removed as ${mentionedIds.length === 1 ? "a completer" : "completers"} of this bounty.` });
+					posting.send({ content: `${listifyEN(hunterIds.map(id => `<@${id}>`))} ${hunterIds.length === 1 ? "has" : "have"} been removed as ${hunterIds.length === 1 ? "a completer" : "completers"} of this bounty.` });
 				});
 			}
 
-			interaction.reply({ content: `The following bounty hunters have been removed as completers from **${bounty.title}**: ${listifyEN(mentionedIds.map(id => userMention(id)))}`, flags: [MessageFlags.Ephemeral] });
+			interaction.reply({ content: `The following bounty hunters have been removed as completers from **${bounty.title}**: ${listifyEN(hunterIds.map(id => userMention(id)))}`, flags: [MessageFlags.Ephemeral] });
 		})
 	}
 ).setOptions(
 	{
 		type: "Integer",
 		name: "bounty-slot",
-		description: "The slot number of the bounty from which to remove completers",
+		description: "The slot number of your bounty",
 		required: true
 	},
 	{
 		type: "String",
-		name: "hunters",
-		description: "The bounty hunter(s) to remove",
+		name: "bounty-hunter",
+		description: "A bounty hunter to uncredit",
 		required: true
+	},
+	{
+		type: "String",
+		name: "second-bounty-hunter",
+		description: "A bounty hunter to uncredit",
+		required: false
+	},
+	{
+		type: "String",
+		name: "third-bounty-hunter",
+		description: "A bounty hunter to uncredit",
+		required: false
+	},
+	{
+		type: "String",
+		name: "fourth-bounty-hunter",
+		description: "A bounty hunter to uncredit",
+		required: false
+	},
+	{
+		type: "String",
+		name: "fifth-bounty-hunter",
+		description: "A bounty hunter to uncredit",
+		required: false
 	}
 );
