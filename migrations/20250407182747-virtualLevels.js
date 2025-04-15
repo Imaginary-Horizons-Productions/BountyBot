7b025b2baf812b76f819c1898b6f3e99f34e0467
Nathaniel Tseng <nathanieltseng@gmail.com>
2025-04-15 09:35:48 -0700

    Update migrations/20250407182747-virtualLevels.js
    Co-authored-by: SillySalamndr <l.ensign@hotmail.com>

--- migrations/20250407182747-virtualLevels.js
+++ migrations/20250407182747-virtualLevels.js
@@ -11,28 +11,31 @@ module.exports = {
 		}
 	},
 	async down(queryInterface, Sequelize) {
+		//region structure
 		const hunterTableDescription = await queryInterface.describeTable("Hunter");
 		if (!("level" in hunterTableDescription)) {
 			await queryInterface.addColumn("Hunter", "level", { type: Sequelize.BIGINT, defaultValue: 1 });
 		}
-		const [hunters] = await queryInterface.sequelize.query("SELECT * FROM Hunter;");
-		const companyXPCoefficientMap = {};
-		for (const hunter of hunters) {
-			if (!(hunter.companyId in companyXPCoefficientMap)) {
-				const [result] = await queryInterface.sequelize.query(`SELECT xpCoefficient FROM Company WHERE id IS ${hunter.companyId};`);
-				companyXPCoefficientMap[hunter.companyId] = result[0].xpCoefficient;
-			}
-			await queryInterface.sequelize.query(`UPDATE Hunter SET level = ${Math.floor(Math.sqrt(hunter.xp / companyXPCoefficientMap[hunter.companyId]) + 1)} WHERE userId is ${hunter.userId} AND companyId is ${hunter.companyId}`);
-		}
-
 		const companyTableDescription = await queryInterface.describeTable("Company");
 		if (!("level" in companyTableDescription)) {
 			await queryInterface.addColumn("Company", "level", { type: Sequelize.INTEGER, defaultValue: 1 });
 		}
+		//endregion
+		//region data
+		const [hunters] = await queryInterface.sequelize.query("SELECT * FROM Hunter;");
 		const [companies] = await queryInterface.sequelize.query("SELECT * FROM Company;");
-		for (const company of companies) {
-			const [result] = await queryInterface.sequelize.query(`SELECT SUM(level) FROM Hunter WHERE companyId IS ${company.id};`);
-			await queryInterface.sequelize.query(`UPDATE Company SET level = ${Math.floor(Math.sqrt(result[0]["SUM(level)"] / 3) + 1)} WHERE id IS ${company.id}`);
+		const companyXPCoefficientMap = {};
+		for (const company of companies) { // Set up XP map
+			companyXPCoefficientMap[company.id] = company.xpCoefficient;
 		}
+		for (const hunter of hunters) { // Set hunter levels
+			await queryInterface.sequelize.query(`UPDATE Hunter SET level = ${xpToLevel(hunter.xp, companyXPCoefficientMap[hunter.companyId])} WHERE userId is ${hunter.userId} AND companyId is ${hunter.companyId}`);
+		}
+
+		for (const company of companies) { // Set company levels
+			const [result] = await queryInterface.sequelize.query(`SELECT SUM(level) FROM Hunter WHERE companyId IS ${company.id};`);
+			await queryInterface.sequelize.query(`UPDATE Company SET level = ${xpToLevel(result[0]["SUM(level)"])} WHERE id IS ${company.id}`);
+		}
+		//endregion
 	}
 };
