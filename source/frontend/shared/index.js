6f5868eaf83e27ba2f3fc4810a606a9df9074d14
Nathaniel Tseng <nathanieltseng@gmail.com>
2025-04-27 11:30:10 -0700

    Update source/frontend/shared/index.js
    Co-authored-by: SillySalamndr <l.ensign@hotmail.com>

--- source/frontend/shared/index.js
+++ source/frontend/shared/index.js
@@ -1,40 +1,6 @@
-const { createSubcommandMappings, bountiesToSelectOptions, trimForModalTitle, trimForSelectOptionDescription, generateBountyBoardThread, updatePosting, updateScoreboard } = require("./dAPIRequests");
-const { commandMention, congratulationBuilder, generateTextBar, getNumberEmoji, listifyEN, ihpAuthorPayload, randomFooterTip, buildVersionEmbed, buildBountyEmbed, generateBountyBoardButtons, generateBountyRewardString, sendAnnouncement, buildCompanyLevelUpLine, seasonalScoreboardEmbed, overallScoreboardEmbed, statsEmbed, getHunterLevelUpRewards, buildHunterLevelUpLine, modStatsEmbed, generateToastEmbed, generateSecondingActionRow, generateToastRewardString, generateCompletionEmbed, generateSecondingRewardString } = require("./messageParts");
-const { getRankUpdates } = require("./toBeMoved");
-const { textsHaveAutoModInfraction } = require("./validations");
-
 module.exports = {
-	generateBountyBoardThread,
-	createSubcommandMappings,
-	bountiesToSelectOptions,
-	trimForModalTitle,
-	trimForSelectOptionDescription,
-	ihpAuthorPayload,
-	randomFooterTip,
-	buildBountyEmbed,
-	buildVersionEmbed,
-	generateBountyRewardString,
-	generateBountyBoardButtons,
-	commandMention,
-	congratulationBuilder,
-	generateTextBar,
-	getNumberEmoji,
-	listifyEN,
-	sendAnnouncement,
-	buildCompanyLevelUpLine,
-	seasonalScoreboardEmbed,
-	overallScoreboardEmbed,
-	statsEmbed,
-	getHunterLevelUpRewards,
-	buildHunterLevelUpLine,
-	modStatsEmbed,
-	generateToastEmbed,
-	generateSecondingActionRow,
-	generateToastRewardString,
-	generateCompletionEmbed,
-	generateSecondingRewardString,
-	textsHaveAutoModInfraction,
-	updatePosting,
-	updateScoreboard,
-	getRankUpdates
+	...require("./dAPIRequests"),
+	...require("./messageParts"),
+	...require("./toBeMoved"),
+	...require("./validations")
 };
