61a3423e96d000e06723cc3bd94d44912fb4f530
Nathaniel Tseng <nathanieltseng@gmail.com>
2025-05-09 15:57:48 -0700

    implement updateCompanyPlacementsAndRanks in `/moderation take-down`

--- source/frontend/commands/moderation/takedown.js
+++ source/frontend/commands/moderation/takedown.js
@@ -1,7 +1,7 @@
 const { ActionRowBuilder, StringSelectMenuBuilder, MessageFlags, ComponentType, DiscordjsErrorCodes } = require("discord.js");
 const { SubcommandWrapper } = require("../../classes");
 const { SAFE_DELIMITER, SKIP_INTERACTION_HANDLING } = require("../../../constants");
-const { getRankUpdates, bountiesToSelectOptions } = require("../../shared");
+const { bountiesToSelectOptions } = require("../../shared");
 
 module.exports = new SubcommandWrapper("take-down", "Take down another user's bounty",
 	async function executeSubcommand(interaction, runMode, ...[logicLayer]) {
@@ -42,8 +42,8 @@ module.exports = new SubcommandWrapper("take-down", "Take down another user's bo
 				logicLayer.hunters.findOneHunter(posterId, interaction.guild.id).then(async poster => {
 					poster.decrement("xp");
 					const [season] = await logicLayer.seasons.findOrCreateCurrentSeason(interaction.guildId);
-					logicLayer.seasons.changeSeasonXP(posterId, interaction.guildId, season.id, -1);
-					getRankUpdates(interaction.guild, logicLayer);
+					logicLayer.seasons.changeSeasonXP(posterId, interaction.guildId, season.id, -1); //TODONOW check for async problems
+					logicLayer.seasons.updateCompanyPlacementsAndRanks(season, await logicLayer.seasons.getCompanyParticipationMap(season.id), await logicLayer.ranks.findAllRanks(interaction.guild.id));
 				})
 				collectedInteraction.reply({ content: `<@${posterId}>'s bounty **${bounty.title}** has been taken down by ${interaction.member}.` });
 			});
