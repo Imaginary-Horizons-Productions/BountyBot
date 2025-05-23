0e2b9ae9f5e6fb7b9742dafb76ae8f39e63e675f
Nathaniel Tseng <nathanieltseng@gmail.com>
2025-05-23 14:13:04 -0700

    outline migration

---
+++ migrations/20250523210528-fractionalFestivalMultipliers.js
@@ -0,0 +1,17 @@
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+	async up(queryInterface, Sequelize) {
+		//TODONOW create placeholder column
+		//TODONOW copy festivalMultiplier to placeholder column
+		//TODONOW delete old column
+		//TODONOW make new column with same name and type REAL
+		//TODONOW copy values from placeholder column to new column
+	},
+	async down(queryInterface, Sequelize) {
+		//TODONOW create placeholder column
+		//TODONOW copy festivalMultiplier to placeholder column
+		//TODONOW delete old column
+		//TODONOW make new column with same name and type INTEGER
+		//TODONOW copy ceilinged values from placeholder column to new column
+	}
+};
