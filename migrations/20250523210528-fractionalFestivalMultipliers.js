feaa420e67b86f4b5f3ce6d2886bd11805ab7fde
Nathaniel Tseng <nathanieltseng@gmail.com>
2025-05-26 13:02:39 -0700

    write migration

--- migrations/20250523210528-fractionalFestivalMultipliers.js
+++ migrations/20250523210528-fractionalFestivalMultipliers.js
@@ -1,17 +1,20 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
 	async up(queryInterface, Sequelize) {
-		//TODONOW create placeholder column
-		//TODONOW copy festivalMultiplier to placeholder column
-		//TODONOW delete old column
-		//TODONOW make new column with same name and type REAL
-		//TODONOW copy values from placeholder column to new column
+		const [companies] = await queryInterface.sequelize.query("SELECT * from Company;");
+		await queryInterface.sequelize.query("ALTER TABLE Company DROP COLUMN festivalMultiplier");
+		await queryInterface.addColumn("Company", "festivalMultiplier", Sequelize.REAL, { defaultValue: 1 });
+		for (const company of companies) {
+			await queryInterface.sequelize.query(`UPDATE Company SET festivalMultiplier = ${company.festivalMultiplier} WHERE id = ${company.id};`);
+		}
 	},
 	async down(queryInterface, Sequelize) {
-		//TODONOW create placeholder column
-		//TODONOW copy festivalMultiplier to placeholder column
-		//TODONOW delete old column
-		//TODONOW make new column with same name and type INTEGER
-		//TODONOW copy ceilinged values from placeholder column to new column
+		const [companies] = await queryInterface.sequelize.query("SELECT * from Company;");
+		console.log(companies);
+		await queryInterface.sequelize.query("ALTER TABLE Company DROP COLUMN festivalMultiplier");
+		await queryInterface.addColumn("Company", "festivalMultiplier", Sequelize.INTEGER, { defaultValue: 1 });
+		for (const company of companies) {
+			await queryInterface.sequelize.query(`UPDATE Company SET festivalMultiplier = ${Math.ceil(company.festivalMultiplier)} WHERE id = ${company.id};`);
+		}
 	}
 };
