ad774461e1a26badd9f14a04f2a31532080ed79d
Nathaniel Tseng <nathanieltseng@gmail.com>
2026-01-12 16:48:20 -0800

    move randomFooterTip to dAPISerialization

--- source/frontend/shared/dAPISerializers.js
+++ source/frontend/shared/dAPISerializers.js
@@ -2,7 +2,7 @@ const { SelectMenuLimits, MessageLimits } = require("@sapphire/discord.js-utilit
 const { truncateTextToLength } = require("./messageParts");
 const { Bounty, Rank } = require("../../database/models");
 const { Role, Collection, AttachmentBuilder, ActionRowBuilder, UserSelectMenuBuilder } = require("discord.js");
-const { SKIP_INTERACTION_HANDLING } = require("../../constants");
+const { SKIP_INTERACTION_HANDLING, bountyBotIconURL, discordIconURL } = require("../../constants");
 const { emojiFromNumber } = require("./stringConstructors");
 
 /** @file Discord API (dAPI) Serializers - changes our data into the shapes dAPI wants */
@@ -39,6 +39,42 @@ function attachOverflowingContentAsFile(content, messageOptions, filename) {
 
 //#region Serializers - returns whole entities
 // Naming Convention: `${outputType}From${inputType}`
+
+/** @type {import("discord.js").EmbedFooterData[]} */
+const discordTips = [
+	"Message starting with @silent don't send notifications; good for when everyone's asleep.",
+	"Surround your message with || to mark it a spoiler (not shown until reader clicks on it).",
+	"Surround a part of your messag with ~~ to add strikethrough styling.",
+	"Don't forget to check slash commands for optional arguments.",
+	"Some slash commands can be used in DMs, others can't.",
+	"Server subscriptions cost more on mobile because the mobile app stores take a cut."
+].map(text => ({ text, iconURL: discordIconURL }));
+/** @type {import("discord.js").EmbedFooterData[]} */
+const bountyBotTips = [
+	"You can showcase one of your bounties once a week to increase its rewards.",
+	"Send bug reports or feature requests with the \"/feedback\".",
+	"Bounties can't be completed until 5 minutes after they've been posted. Don't make them too easy!",
+	"You get XP for posting a bounty, but lose that XP if it's taken down before it's completed.",
+	"You get XP when your bounties are completed. Thanks for posting!",
+	"You get more XP when a bigger group completes your bounties. Thanks for organizing!",
+	"Sometimes when you raise a toast to someone, it'll crit and grant you XP too!",
+	"Your chance for Critical Toast is lower when repeatedly toasting the same bounty hunters. Spread the love!",
+	"Users who can manage BountyBot aren't included in seasonal rewards to avoid conflicts of interest.",
+	"Anyone can post a bounty, even you!",
+	"Anyone can raise a toast, even you!",
+	"The Overjustification Effect means a small reward can be less motivating than no reward.",
+	"Manage bounties from within games with the Discord Overlay (default: Shift + Tab)!",
+	"Server level is based on total bounty hunter level--higher server level means better evergreen bounty rewards.",
+	"A bounty poster cannot complete their own bounty.",
+	"Adding a description, image or time to a bounty all add 1 bonus XP for the poster.",
+	"Bounty posters have double the chance to find items compared to completers."
+].map(text => ({ text, iconURL: bountyBotIconURL }));
+const tipPool = bountyBotTips.concat(bountyBotTips, discordTips);
+
+/** twice as likely to roll an application specific tip as a discord tip */
+function randomFooterTip() {
+	return tipPool[Math.floor(Math.random() * tipPool.length)];
+}
 
 /** @param {string} placeholderText */
 function disabledSelectRow(placeholderText) {
@@ -86,7 +122,8 @@ function selectOptionsFromRanks(ranks, allGuildRoles) {
 module.exports = {
 	truncateTextToLength,
 	attachOverflowingContentAsFile,
+	randomFooterTip,
 	disabledSelectRow,
 	selectOptionsFromBounties,
-	selectOptionsFromRanks
+	selectOptionsFromRanks,
 }
--- source/frontend/shared/messageParts.js
+++ source/frontend/shared/messageParts.js
@@ -1,47 +1,9 @@
 const fs = require("fs");
 const { EmbedBuilder, Colors, Guild, ActionRowBuilder, ButtonBuilder, ButtonStyle, heading, userMention, MessageFlags, bold, italic, GuildMember, Role, Collection, StringSelectMenuBuilder, GuildScheduledEventPrivacyLevel, GuildScheduledEventEntityType, unorderedList, TextInputBuilder, TextInputStyle, ModalBuilder, LabelBuilder, UserSelectMenuBuilder, underline } = require("discord.js");
 const { MessageLimits, EmbedLimits, ModalLimits, SelectMenuLimits } = require("@sapphire/discord.js-utilities");
-const { SAFE_DELIMITER, COMPANY_XP_COEFFICIENT, commandIds, YEAR_IN_MS, SKIP_INTERACTION_HANDLING } = require("../../constants");
+const { SAFE_DELIMITER, COMPANY_XP_COEFFICIENT, commandIds, YEAR_IN_MS, SKIP_INTERACTION_HANDLING, discordIconURL, bountyBotIconURL } = require("../../constants");
 const { Bounty, Completion, Company, Season, Rank, Participation, Hunter } = require("../../database/models");
 const { descendingByProperty, discordTimestamp, timeConversion } = require("../../shared");
-
-const discordIconURL = "https://cdn.discordapp.com/attachments/618523876187570187/1110265047516721333/discord-mark-blue.png";
-const bountyBotIcon = "https://cdn.discordapp.com/attachments/618523876187570187/1138968614364528791/BountyBotIcon.jpg";
-/** @type {import("discord.js").EmbedFooterData[]} */
-const discordTips = [
-	"Message starting with @silent don't send notifications; good for when everyone's asleep.",
-	"Surround your message with || to mark it a spoiler (not shown until reader clicks on it).",
-	"Surround a part of your messag with ~~ to add strikethrough styling.",
-	"Don't forget to check slash commands for optional arguments.",
-	"Some slash commands can be used in DMs, others can't.",
-	"Server subscriptions cost more on mobile because the mobile app stores take a cut."
-].map(text => ({ text, iconURL: discordIconURL }));
-/** @type {import("discord.js").EmbedFooterData[]} */
-const bountyBotTips = [
-	"You can showcase one of your bounties once a week to increase its rewards.",
-	"Send bug reports or feature requests with the \"/feedback\".",
-	"Bounties can't be completed until 5 minutes after they've been posted. Don't make them too easy!",
-	"You get XP for posting a bounty, but lose that XP if it's taken down before it's completed.",
-	"You get XP when your bounties are completed. Thanks for posting!",
-	"You get more XP when a bigger group completes your bounties. Thanks for organizing!",
-	"Sometimes when you raise a toast to someone, it'll crit and grant you XP too!",
-	"Your chance for Critical Toast is lower when repeatedly toasting the same bounty hunters. Spread the love!",
-	"Users who can manage BountyBot aren't included in seasonal rewards to avoid conflicts of interest.",
-	"Anyone can post a bounty, even you!",
-	"Anyone can raise a toast, even you!",
-	"The Overjustification Effect means a small reward can be less motivating than no reward.",
-	"Manage bounties from within games with the Discord Overlay (default: Shift + Tab)!",
-	"Server level is based on total bounty hunter level--higher server level means better evergreen bounty rewards.",
-	"A bounty poster cannot complete their own bounty.",
-	"Adding a description, image or time to a bounty all add 1 bonus XP for the poster.",
-	"Bounty posters have double the chance to find items compared to completers."
-].map(text => ({ text, iconURL: bountyBotIcon }));
-const tipPool = bountyBotTips.concat(bountyBotTips, discordTips);
-
-/** twice as likely to roll an application specific tip as a discord tip */
-function randomFooterTip() {
-	return tipPool[Math.floor(Math.random() * tipPool.length)];
-}
 
 /** Generate an embed for the given bounty
  * @param {Bounty} bounty
