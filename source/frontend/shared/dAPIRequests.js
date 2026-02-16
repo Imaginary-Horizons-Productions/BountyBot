842cca0384ae26029662d8644a2081e791425295
Nathaniel Tseng <nathanieltseng@gmail.com>
2026-02-16 14:50:08 -0800

    reduce PR changes

--- source/frontend/shared/dAPIRequests.js
+++ source/frontend/shared/dAPIRequests.js
@@ -1,6 +1,6 @@
 const { GuildTextThreadManager, EmbedBuilder, Guild, MessageFlags, Message, GuildMemberManager, ForumChannel, ThreadChannel } = require("discord.js");
 const { Bounty, Company, Rank, Participation } = require("../../database/models");
-const { bountyEmbed, seasonalScoreboardEmbed, overallScoreboardEmbed } = require("./dAPISerializers");
+const { bountyEmbed, overallScoreboardEmbed, seasonalScoreboardEmbed } = require("./dAPISerializers");
 const { ascendingByProperty } = require("../../shared");
 
 /**
