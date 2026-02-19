c4a254c80c12f17bbeb7280b7a9c4454b859a55e
Nathaniel Tseng <nathanieltseng@gmail.com>
2026-02-19 10:52:57 -0800

    #554: upgrade Bounty Thumbnail item ui - bounty select in modal - file upload instead of url text input

--- source/frontend/items/bounty-thumbnail.js
+++ source/frontend/items/bounty-thumbnail.js
@@ -1,4 +1,4 @@
-const { ActionRowBuilder, StringSelectMenuBuilder, ModalBuilder, TextInputBuilder, TextInputStyle, MessageFlags, ComponentType, LabelBuilder } = require("discord.js");
+const { StringSelectMenuBuilder, ModalBuilder, MessageFlags, LabelBuilder, FileUploadBuilder, channelMention } = require("discord.js");
 const { ItemTemplate, ItemTemplateSet } = require("../classes");
 const { SKIP_INTERACTION_HANDLING } = require("../../constants");
 const { selectOptionsFromBounties, refreshBountyThreadStarterMessage, butIgnoreInteractionCollectorErrors } = require("../shared");
@@ -9,7 +9,7 @@ let logicLayer;
 
 const itemName = "Bounty Thumbnail";
 module.exports = new ItemTemplateSet(
-	new ItemTemplate(itemName, "Adds an image (via URL) to one of your open bounties!", 3000,
+	new ItemTemplate(itemName, "Adds an image to one of your open bounties!", 3000,
 		async (interaction, origin) => {
 			const openBounties = await logicLayer.bounties.findOpenBounties(interaction.user.id, interaction.guild.id);
 			if (openBounties.length < 1) {
@@ -20,52 +20,37 @@ module.exports = new ItemTemplateSet(
 				new ModalBuilder().setCustomId(`${SKIP_INTERACTION_HANDLING}${interaction.id}`)
 					.setTitle("Add Bounty Thumbnail")
 					.addLabelComponents(
+						new LabelBuilder().setLabel("Bounty")
+							.setStringSelectMenuComponent(
+								new StringSelectMenuBuilder().setCustomId("bounty-id")
+									.setPlaceholder("Select a bounty...")
+									.setOptions(selectOptionsFromBounties(openBounties))
+							),
 						new LabelBuilder().setLabel("Image URL")
-							.setTextInputComponent(
-								new TextInputBuilder().setCustomId("imageURL")
-									.setStyle(TextInputStyle.Short)
+							.setFileUploadComponent(
+								new FileUploadBuilder().setCustomId("imageURL")
+									.setMaxValues(1)
 							)
 					)
 			);
 
 			return interaction.awaitModalSubmit({ filter: (incoming) => incoming.customId === `${SKIP_INTERACTION_HANDLING}${interaction.id}`, time: timeConversion(5, "m", "ms") }).then(async modalSubmission => {
-				const imageURL = modalSubmission.fields.getTextInputValue("imageURL");
-				if (imageURL) {
-					try {
-						new URL(imageURL);
-					} catch (error) {
-						interaction.reply({ content: `${imageURL} is not usable as a URL for a bounty thumbnail.`, flags: MessageFlags.Ephemeral });
-						return true;
-					}
+				const bounty = await openBounties.find(bounty => bounty.id === modalSubmission.fields.getStringSelectValues("bounty-id")[0]).reload();
+				if (bounty?.state !== "open") {
+					return modalSubmission.reply({ content: "The selected bounty does not seem to be open.", flags: MessageFlags.Ephemeral });
 				}
 
-				return modalSubmission.reply({
-					content: `A bounty's thumbnail will be shown on the bounty board and in the bounty's embed. It will also increase the XP you receive when the bounty's completed by 1.\n${imageURL}`,
-					components: [
-						new ActionRowBuilder().addComponents(
-							new StringSelectMenuBuilder().setCustomId(SKIP_INTERACTION_HANDLING)
-								.setPlaceholder("Select a bounty...")
-								.setOptions(selectOptionsFromBounties(openBounties))
-						)
-					],
-					flags: MessageFlags.Ephemeral,
-					withResponse: true
-				}).then(response => response.resource.message.awaitMessageComponent({ time: 120000, componentType: ComponentType.StringSelect })).then(async collectedInteraction => {
-					const bounty = await openBounties.find(bounty => bounty.id === collectedInteraction.values[0]).reload();
-					if (bounty?.state !== "open") {
-						return collectedInteraction.reply({ content: "The selected bounty does not seem to be open.", flags: MessageFlags.Ephemeral });
-					}
-					bounty.thumbnailURL = imageURL;
-					await bounty.save().then(async bounty => {
-						bounty.reload();
-						const company = await logicLayer.companies.findCompanyByPK(interaction.guildId);
-						refreshBountyThreadStarterMessage(interaction.guild, company, bounty, (await logicLayer.hunters.findOneHunter(interaction.user.id, interaction.guild.id)).getLevel(company.xpCoefficient), await logicLayer.bounties.getHunterIdSet(bounty.id));
-					});
-					return collectedInteraction.reply({ content: `The thumbnail on ${bounty.title} has been updated.${bounty.postingId !== null ? ` <#${bounty.postingId}>` : ""}`, flags: MessageFlags.Ephemeral });
-				})
-			}).catch(butIgnoreInteractionCollectorErrors).finally(() => {
-				modalSubmission.deleteReply();
-			})
+				const imageFileCollection = modalSubmission.fields.getUploadedFiles("imageURL", true);
+				const firstAttachment = imageFileCollection.first();
+				if (!firstAttachment) {
+					return modalSubmission.reply({ content: "There was an error handling the submitted image.", flags: MessageFlags.Ephemeral });
+				}
+
+				await bounty.update({ thumbnailURL: firstAttachment.url }).then(async bounty => {
+					refreshBountyThreadStarterMessage(interaction.guild, origin.company, bounty, (await logicLayer.hunters.findOneHunter(interaction.user.id, interaction.guild.id)).getLevel(origin.company.xpCoefficient), await logicLayer.bounties.getHunterIdSet(bounty.id));
+				});
+				return modalSubmission.reply({ content: `The thumbnail on ${bounty.title} has been updated.${bounty.postingId !== null ? ` ${channelMention(bounty.postingId)}` : ""}`, flags: MessageFlags.Ephemeral });
+			}).catch(butIgnoreInteractionCollectorErrors);
 		}
 	)
 ).setLogicLinker(logicBlob => {
