8f8db1bda04f965358b1ce258e9990647a72f66a
Nathaniel Tseng <nathanieltseng@gmail.com>
2026-02-21 15:57:35 -0800

    use constants for label input ids - stop calling image "imageURL"

--- source/frontend/commands/bounty/post.js
+++ source/frontend/commands/bounty/post.js
@@ -59,40 +59,45 @@ module.exports = new SubcommandWrapper("post", "Post your own bounty (+1 XP)",
 				return;
 			}
 
+			const titleId = "title";
+			const descriptionId = "description";
+			const startTimestampId = "startTimestamp";
+			const endTimestampId = "endTimestamp";
+			const imageId = "image";
 			const modal = new ModalBuilder().setCustomId(`${SKIP_INTERACTION_HANDLING}${collectedInteraction.id}`)
 				.setTitle(`New Bounty (Slot ${slotNumber})`)
 				.addLabelComponents(
 					new LabelBuilder().setLabel("Title")
 						.setTextInputComponent(
-							new TextInputBuilder().setCustomId("title")
+							new TextInputBuilder().setCustomId(titleId)
 								.setStyle(TextInputStyle.Short)
 								.setPlaceholder("Discord markdown allowed...")
 								.setMaxLength(EmbedLimits.MaximumTitleLength)
 						),
 					new LabelBuilder().setLabel("Description")
 						.setTextInputComponent(
-							new TextInputBuilder().setCustomId("description")
+							new TextInputBuilder().setCustomId(descriptionId)
 								.setRequired(false)
 								.setStyle(TextInputStyle.Paragraph)
 								.setPlaceholder("Get a 1 XP bonus on completion for the following: description, image URL, timestamps")
 						),
 					new LabelBuilder().setLabel("Event Start (Unix Timestamp)")
 						.setTextInputComponent(
-							new TextInputBuilder().setCustomId("startTimestamp")
+							new TextInputBuilder().setCustomId(startTimestampId)
 								.setRequired(false)
 								.setStyle(TextInputStyle.Short)
 								.setPlaceholder("Required if making an event with the bounty")
 						),
 					new LabelBuilder().setLabel("Event End (Unix Timestamp)")
 						.setTextInputComponent(
-							new TextInputBuilder().setCustomId("endTimestamp")
+							new TextInputBuilder().setCustomId(endTimestampId)
 								.setRequired(false)
 								.setStyle(TextInputStyle.Short)
 								.setPlaceholder("Required if making an event with the bounty")
 						),
 					new LabelBuilder().setLabel("Image")
 						.setFileUploadComponent(
-							new FileUploadBuilder().setCustomId("imageURL")
+							new FileUploadBuilder().setCustomId(imageId)
 								.setMaxValues(1)
 								.setRequired(false)
 						)
@@ -100,8 +105,8 @@ module.exports = new SubcommandWrapper("post", "Post your own bounty (+1 XP)",
 			collectedInteraction.showModal(modal);
 
 			return collectedInteraction.awaitModalSubmit({ filter: (incoming) => incoming.customId === modal.data.custom_id, time: timeConversion(5, "m", "ms") }).then(async modalSubmission => {
-				const title = modalSubmission.fields.getTextInputValue("title");
-				const description = modalSubmission.fields.getTextInputValue("description");
+				const title = modalSubmission.fields.getTextInputValue(titleId);
+				const description = modalSubmission.fields.getTextInputValue(descriptionId);
 
 				const autoModInfraction = await textsHaveAutoModInfraction(modalSubmission.channel, modalSubmission.member, [title, description], "bounty post");
 				if (autoModInfraction == null) {
@@ -123,7 +128,7 @@ module.exports = new SubcommandWrapper("post", "Post your own bounty (+1 XP)",
 				}
 				const errors = [];
 
-				const attachmentFileCollection = modalSubmission.fields.getUploadedFiles("imageURL");
+				const attachmentFileCollection = modalSubmission.fields.getUploadedFiles(imageId);
 				if (attachmentFileCollection) {
 					const firstAttachment = attachmentFileCollection.first();
 					if (firstAttachment) {
@@ -131,8 +136,8 @@ module.exports = new SubcommandWrapper("post", "Post your own bounty (+1 XP)",
 					}
 				}
 
-				const startTimestamp = parseInt(modalSubmission.fields.getTextInputValue("startTimestamp"));
-				const endTimestamp = parseInt(modalSubmission.fields.getTextInputValue("endTimestamp"));
+				const startTimestamp = parseInt(modalSubmission.fields.getTextInputValue(startTimestampId));
+				const endTimestamp = parseInt(modalSubmission.fields.getTextInputValue(endTimestampId));
 				if (startTimestamp || endTimestamp) {
 					errors.push(...validateScheduledEventTimestamps(startTimestamp, endTimestamp))
 				}
