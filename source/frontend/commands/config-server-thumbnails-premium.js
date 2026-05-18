b4a479d1f26efcd2e395d489ead42f5ccdfed3ee
Nathaniel Tseng <nathanieltseng@gmail.com>
2026-05-18 07:58:13 -0700

    #636: use Container to show labeled received images in thumbnail config commands (#673)

--- source/frontend/commands/config-server-thumbnails-premium.js
+++ source/frontend/commands/config-server-thumbnails-premium.js
@@ -1,59 +1,27 @@
-const { PermissionFlagsBits, InteractionContextType, MessageFlags, ModalBuilder, LabelBuilder, FileUploadBuilder } = require("discord.js");
-const { SKIP_INTERACTION_HANDLING } = require('../../constants');
+const { PermissionFlagsBits, InteractionContextType } = require("discord.js");
 const { CommandWrapper } = require("../classes");
-const { timeConversion } = require('../../shared');
+const { configCompanyThumbnails } = require("../shared/flows/configCompanyThumbnails");
 
 const mainId = "config-server-thumbnails-premium";
-const imageFieldToPayloadPropertyAndMessage = new Map(Object.entries({
-	"scoreboard-thumbnail": {
-		payloadProperty: "scoreboardThumbnailURL",
-		messageStub: "scoreboard thumbnail",
+const thumbnailUpdateData = [
+	{
+		label: "Scoreboard Thumbnail",
 		description: "Set an image to use as thumbnail on the scoreboard",
-		modalLabel: "Scoreboard Thumbnail"
+		payloadProperty: "scoreboardThumbnailURL"
 	},
-	"goal-completion-thumbnail": {
-		payloadProperty: "goalCompletionThumbnailURL",
-		messageStub: "goal completion thumbnail",
+	{
+		label: "Goal Completion Thumbnail",
 		description: "Set an image to use as thumbnail in server goal completion messages",
-		modalLabel: "Goal Completion Thumbnail"
+		payloadProperty: "goalCompletionThumbnailURL"
 	},
-	"raffle-thumbnail": {
-		payloadProperty: "raffleThumbnailURL",
-		messageStub: "raffle thumbnail",
+	{
+		label: "Raffle Thumbnail",
 		description: "Set an image to use as thumbnail in raffle winner messages",
-		modalLabel: "Raffle Thumbnail"
+		payloadProperty: "raffleThumbnailURL"
 	}
-}));
+];
 module.exports = new CommandWrapper(mainId, "Configure thumbnails for server messages (Premium)", PermissionFlagsBits.ManageGuild, true, [InteractionContextType.Guild], 3000,
 	async (interaction, origin, runMode) => {
-		const modalLabelComponents = [];
-		for (const [imageField, { description, modalLabel }] of imageFieldToPayloadPropertyAndMessage) {
-			modalLabelComponents.push(new LabelBuilder().setLabel(modalLabel).setDescription(description)
-				.setFileUploadComponent(
-					new FileUploadBuilder().setCustomId(imageField)
-						.setRequired(false)
-				))
-		}
-		const modal = new ModalBuilder().setCustomId(`${SKIP_INTERACTION_HANDLING}${interaction.id}`).setTitle("Configure Server Thumbnails").addLabelComponents(modalLabelComponents);
-
-		interaction.showModal(modal);
-		const modalInteraction = await interaction.awaitModalSubmit({ filter: incoming => incoming.customId === modal.data.custom_id, time: timeConversion(5, "m", "ms") });
-		const updatePayload = {};
-
-		let replyContent = "The following thumbnails have been configured:";
-
-		for (const [imageField, { payloadProperty, messageStub }] of imageFieldToPayloadPropertyAndMessage) {
-			const thumbnailCollection = modalInteraction.fields.getUploadedFiles(imageField);
-			if (thumbnailCollection) {
-				const firstAttachment = thumbnailCollection.first();
-				if (firstAttachment) {
-					updatePayload[payloadProperty] = firstAttachment.url;
-					replyContent += `\n- The ${messageStub} was set to <${firstAttachment.url}>.`;
-				}
-			}
-		}
-
-		origin.company.update(updatePayload);
-		modalInteraction.reply({ content: replyContent, flags: MessageFlags.Ephemeral });
+		configCompanyThumbnails("Server Message Thumbnail", thumbnailUpdateData, interaction, origin.company);
 	}
 );
