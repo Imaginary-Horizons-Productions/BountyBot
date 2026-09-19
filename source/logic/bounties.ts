557830634949e7353f676d72244a63ebb9322542
Nathaniel Tseng <nathanieltseng@gmail.com>
2026-09-19 12:21:36 -0700

    ensure null doesn't get added to return of `getHunterIdSet`

--- source/logic/bounties.ts
+++ source/logic/bounties.ts
@@ -61,8 +61,13 @@ export function findBountyCompletions(bountyId: string) {
 
 /** *Get a Set with the userIds of the specified Bounty's hunters* */
 export async function getHunterIdSet(bountyId: string) {
-	const completions = await db.Completions.findAll({ where: { bountyId } });
-	return completions.reduce((set, completion) => set.add(completion.userId), new Set<Snowflake>());
+	const ids = new Set<Snowflake>();
+	for (const completion of await db.Completions.findAll({ where: { bountyId } })) {
+		if (completion.userId) {
+			ids.add(completion.userId);
+		}
+	}
+	return ids;
 }
 
 /** Filter out the Bounty's poster, bots, and banned Hunters */
